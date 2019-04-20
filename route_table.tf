a64ddfbe1fa05ab88839fed317b104b4be322290
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-04-20 17:10:20 -0500

    file

--- route_table.tf
+++ route_table.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "r" {
 
   route {
       cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw1}"
+      gateway_id = "${aws_internet_gateway.igw1.id}"
   }
 
 }
