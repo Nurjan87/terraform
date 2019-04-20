9a65c4707e159f37976fd71c0c8454db4fec03fe
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-04-20 16:27:26 -0500

    file

---
+++ ig.tf
@@ -0,0 +1,9 @@
+resource "aws_internet_gateway" "igw1" {
+    vpc_id = "${aws_vpc.dev.id}"
+    cidr_block = "10.0.1.0/24"
+    tags {
+     Name = "terraform-january-nurjan-${var.Created_by}-${count.index +1}"
+     Env ="${var.Env}"
+     Dept = "${var.Dept}"
+     Created_by = "${var.Created_by}"
+}
