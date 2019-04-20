4925ccd82086f2c75751d8c25c0ecf8095e62231
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-04-20 17:06:25 -0500

    file

--- instance.tf
+++ instance.tf
@@ -4,4 +4,14 @@ resource "aws_instance" "web" {
   subnet_id = "subnet-09cf050f2291537b9"
   count = "${var.count}"
   key_name = "${var.key_name}"
+  subnet_id = "${}"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+
+    tags {
+     Name = "terraform-${var.Created_by}-${count.index +1}"
+     Env ="${var.Env}"
+     Dept = "${var.Dept}"
+     Created_by = "${var.Created_by}"
+  }
 }
