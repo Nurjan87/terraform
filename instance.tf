7d925e05404b420f519c019db91461baf4f92bf5
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-04-20 17:09:20 -0500

    file

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   subnet_id = "subnet-09cf050f2291537b9"
   count = "${var.count}"
   key_name = "${var.key_name}"
-  subnet_id = "${}"
+  subnet_id = "${aws_subnet.dev1.id}"
   associate_public_ip_address = "true"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 
